58c2470712186bed62197f6beb053aadf016d155
Saloni Sharma <sharmasaloni061@gmail.com>
2020-03-16 16:41:33 +0530

    Applied thunk middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export const RECEIVE_MEMES = 'RECEIVE_MEMES';
 
 function receiveMemes(json) {
-    const {memes} = json.data;
+    const { memes } = json.data;
     return {
         type: RECEIVE_MEMES,
         memes
@@ -10,7 +10,7 @@ function receiveMemes(json) {
 
 function fetchMemesJson() {
     return fetch('https://api.imgflip.com/get_memes')
-            .then(response=>response.json)
+            .then(response => response.json())
 }
 
 export function fetchMemes() {
